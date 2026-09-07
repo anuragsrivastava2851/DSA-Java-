6c1f36fbad27ea2a276e30ab6a8a5201f286c3f9
Anurag Srivastava <anuragsrivastava2851@gmail.com>
2026-09-07 15:46:03 +0530

    Add swapping implementation with methods to create an ArrayList and swap elements

---
+++ array_list/swapping.java
@@ -0,0 +1,30 @@
+package array_list;
+
+import java.util.ArrayList;
+
+public class swapping {
+    public static void main(String[] args){
+         ArrayList<Integer> list = new ArrayList<>();
+
+       list.add(1);
+       list.add(2);
+       list.add(7);
+       list.add(4);
+       list.add(5);
+
+       System.out.println(list);
+
+       int index1=1;
+       int index2=3;
+
+       swap(list, index1, index2);
+       System.out.println(list);
+
+    }
+
+    public static void swap(ArrayList<Integer> list, int index1, int index2){
+        int temp= list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
+    }
+}
